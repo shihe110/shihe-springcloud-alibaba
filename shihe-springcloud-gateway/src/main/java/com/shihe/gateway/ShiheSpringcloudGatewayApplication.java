1500e0334290da7514db735f1f1545299bd35c89
shihe <shihe110@126.com>
2022-05-11 10:43:50 +0800

    springcloud gateway

---
+++ shihe-springcloud-gateway/src/main/java/com/shihe/gateway/ShiheSpringcloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shihe.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiheSpringcloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiheSpringcloudGatewayApplication.class, args);
+    }
+
+}
---
+++ shihe-springcloud-gateway/src/test/java/com/shihe/gateway/ShiheSpringcloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shihe.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiheSpringcloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
